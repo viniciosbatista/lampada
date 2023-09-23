d5b0fee121ec0b046cd0be2f7a24d858f46da2bf
Vinicios Batista <113473637+viniciosbatista@users.noreply.github.com>
2023-09-23 00:46:33 -0300

    fim

---
+++ externo.js
@@ -0,0 +1,13 @@
+let botao = document.getElementById('bt1')
+let botao2 = document.getElementById('bt2')
+let image = document.getElementById('img')
+botao.addEventListener('click',clicou)
+botao2.addEventListener('click',clicou2)
+function clicou(){
+    image.src = 'imagens/apagada.gif'
+    document.body.style.backgroundColor = 'black'
+}
+function clicou2(){
+    image.src = 'imagens/acesa.gif'
+    document.body.style.backgroundColor = 'white'
+}
