adeb6566fc77ba8308602a8c2e53721f84e31c46
Vinicios Batista <113473637+viniciosbatista@users.noreply.github.com>
2023-09-25 22:49:07 -0300

    finalizado

--- externo.js
+++ externo.js
@@ -9,5 +9,5 @@ function clicou(){
 }
 function clicou2(){
     image.src = 'imagens/acesa.gif'
-    document.body.style.backgroundColor = 'white'
+    document.body.style.backgroundColor = 'rgba(228, 228, 16, 0.678)'
 }
